0c75ffce84acf46ca986ca218d420fd9befb6927
Harsh <harsh0564.cse19@chitkara.edu.in>
2022-05-09 00:56:23 +0530

    css

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,123 +1,97 @@
-import React from 'react';
-import TestiCard from './TestiCard';
-import { useState, useEffect} from 'react';
+import React from "react";
+import TestiCard from "./TestiCard";
+import { useState, useEffect } from "react";
 
-import { BsChevronDoubleRight,BsChevronDoubleLeft } from 'react-icons/bs';
+import { BsChevronDoubleRight, BsChevronDoubleLeft } from "react-icons/bs";
 
 export default function Testimonials() {
-
-    var arr = [
-        {
-            name:"Harsh",
-            url:"https://randomuser.me/api/portraits/men/9.jpg",
-            body:"“I am very pleased to say that my experience of associating with Sahay Foundation has been excellent and I feel it is a privilege too. My appreciation for the excellent work Sahay is carrying out in uplifting under- privileged children. My good wishes to Sahay to continue reaching out to children who need them the most. keep up the good work.“"
-        }, 
-        {
-            name:"Muskan",
-            url:"https://randomuser.me/api/portraits/women/76.jpg",
-            body:"“It gives me immense pleasure to share my thoughts about Sahay Foundation. The idea to have a realistic NGO strike to me when I came across with underprivileged children who were deprived of basic needs like education, health care and nutrition. In short duration Sahay has done tremendous work for upliftment of underprivileged children.”"
-        },
-         {
-            name:"Maanksh",
-            url:"https://randomuser.me/api/portraits/men/51.jpg",
-            body:"“A NGO that truely works for the upliftment and betterment of society with its vision of considering children as the future of the country.......Great going.......wish you best of luck.......Sahay”"
-        }
-    ];
-
-
-
-
-
+  var arr = [
+    {
+      name: "Harsh",
+      url: "https://randomuser.me/api/portraits/men/9.jpg",
+      body: "“I am very pleased to say that my experience of associating with Sahay Foundation has been excellent and I feel it is a privilege too. My appreciation for the excellent work Sahay is carrying out in uplifting under- privileged children. My good wishes to Sahay to continue reaching out to children who need them the most. keep up the good work.“",
+    },
+    {
+      name: "Muskan",
+      url: "https://randomuser.me/api/portraits/women/76.jpg",
+      body: "“It gives me immense pleasure to share my thoughts about Sahay Foundation. The idea to have a realistic NGO strike to me when I came across with underprivileged children who were deprived of basic needs like education, health care and nutrition. In short duration Sahay has done tremendous work for upliftment of underprivileged children.”",
+    },
+    {
+      name: "Maanksh",
+      url: "https://randomuser.me/api/portraits/men/51.jpg",
+      body: "“A NGO that truely works for the upliftment and betterment of society with its vision of considering children as the future of the country.......Great going.......wish you best of luck.......Sahay”",
+    },
+  ];
 
   var [cIndex, setCIndex] = useState(0);
-  const next = () => 
-  {  
-    
+  const next = () => {
+    if (cIndex >= 2) {
+      setCIndex(0);
+    } else {
+      setCIndex((prev) => prev + 1);
+    }
+  };
 
+  useEffect(() => {
+    var int = setTimeout(() => {
+      next();
+    }, 5000);
 
-      if(cIndex >=2)
-      {
-          setCIndex(0);
-
-        
-      }
-      else{
-          
-setCIndex((prev)=>prev+1);
-      }
-
-  }
-
-  useEffect(()=>
-  {
-
-  var int =   setTimeout(()=>
-  {
-next();
-  },5000);
-
-  return ()=>
-  {
+    return () => {
       clearTimeout(int);
-  }
+    };
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[cIndex]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [cIndex]);
 
+  const previous = () => {
+    console.log(cIndex);
 
-  
-
-  const previous = () => 
-  {  console.log(cIndex)
-
-
-      if(cIndex<=0)
-      {
-          setCIndex(2);
-
-        
-      }
-      else{
-          
-setCIndex((prev)=>prev-1);
-      }
-
-  }
+    if (cIndex <= 0) {
+      setCIndex(2);
+    } else {
+      setCIndex((prev) => prev - 1);
+    }
+  };
 
   return (
     <div className="w-full min-h-50v md:min-h-70v p-8 bg-s-blue ">
+      <h1 className="text-2xl md:text-4xl uppercase underline underline-offset-2 font-extrabold py-4 text-center">
+        Testimonials
+      </h1>
 
-
-<h1 className="text-2xl md:text-4xl uppercase underline underline-offset-2 font-extrabold py-4 text-center">
-           Testimonials
-          </h1>
-
-            <div className="md:w-3/4 w-full md:h-[45vh] pt-3 relative transition mx-auto">
-
-           
-             {
-                 arr.map((val,index)=>
-                    {
-                        return <>
-                        <div className={cIndex===index?'w-full h-full transition mx-auto opacity-100  ease-in-out duration-[2s]  text-s-white justify-center flex flex-col items-center':"absolute left-[-1000%] opacity-0"}><TestiCard name={val.name} url={val.url} body={val.body}/></div>
-                   </>
-                    })
-             }
-           <div className="absolute top-1/2 left-[-3%] z-10">
-
-<BsChevronDoubleLeft onClick={()=>previous()} className='text-s-white cursor-pointer ease-in duration-300 hover:text-s-gray ' size={45}/>
-
- </div>
- <div className="absolute top-1/2 right-[-3%] z-10">
-
-<BsChevronDoubleRight onClick={()=>next()} className='text-s-white cursor-pointer ease-in duration-300 hover:text-s-gray ' size={45}/>
-
- </div>
-             </div>
-
-       
-
-        
+      <div className="md:w-3/4 w-full md:h-[45vh] pt-3 relative transition mx-auto">
+        {arr.map((val, index) => {
+          return (
+            <>
+              <div
+                key={index}
+                className={
+                  cIndex === index
+                    ? "w-full h-full transition mx-auto opacity-100  ease-in-out duration-[2s]  text-s-white justify-center flex flex-col items-center"
+                    : "absolute left-[-1000%] opacity-0"
+                }
+              >
+                <TestiCard name={val.name} url={val.url} body={val.body} />
+              </div>
+            </>
+          );
+        })}
+        <div className="absolute top-1/2 left-[-3%] z-10">
+          <BsChevronDoubleLeft
+            onClick={() => previous()}
+            className="text-s-white cursor-pointer ease-in duration-300 hover:text-s-gray "
+            size={45}
+          />
+        </div>
+        <div className="absolute top-1/2 right-[-3%] z-10">
+          <BsChevronDoubleRight
+            onClick={() => next()}
+            className="text-s-white cursor-pointer ease-in duration-300 hover:text-s-gray "
+            size={45}
+          />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
--- src/components/Thanks.js
+++ src/components/Thanks.js
@@ -1,44 +1,42 @@
-import React from 'react';
+import React from "react";
 import { useEffect } from "react";
 import done2 from "../media/done2.gif";
 
-import './fade.css';
+import "./fade.css";
 
-
-export default function Thanks({setShow}) {
- 
-useEffect(() => {
+export default function Thanks({ setShow }) {
+  useEffect(() => {
     document.body.style.overflowY = "hidden";
 
-    let timer = setTimeout(()=>
-    {
+    let timer = setTimeout(() => {
+      setShow(false);
+    }, 4000);
 
-        setShow(false);
+    return () => {
+      document.body.style.overflowY = "scroll";
+      clearTimeout(timer);
+    };
 
-      
-    },4000);
-
-
-
-    return ()=>
-    {
-        document.body.style.overflowY = "scroll";
-        clearTimeout(timer);
-    } 
-    
     // eslint-disable-next-line
   }, []);
- return (
-    <div className='w-screen h-screen fixed top-0 left-0 z-50 bg-[#00000080]  flex justify-center animate  items-center   '>
+  return (
+    <div className="w-screen h-screen fixed top-0 left-0 z-50 bg-[#00000080]  flex justify-center animate  items-center   ">
+      <div
+        id="box"
+        className="w-[80%] h-[30%] md:w-[40%] md:h-[40%] p-2  bg-[#fff] rounded-md transition ease-in-out duration-500 flex   flex-col justify-evenly  items-center"
+      >
+        <h2 className="text-xl md:text-xxl text-blue-600 mg-auto font-extrabold ">
+          Thank you
+        </h2>
+        <img alt="logo" className="w-auto h-[50%]" src={done2} />
 
-<div id="box" className='w-[80%] h-[30%] md:w-[40%] md:h-[40%] p-2  bg-[#fff] rounded-md transition ease-in-out duration-500 flex   flex-col justify-evenly  items-center'>
-<h2 className="text-xl md:text-xxl text-blue-600 mg-auto font-extrabold ">Thank you</h2>
-<img alt="logo"   className="w-auto h-[50%]" src={done2} />
-
-<h3 className="text-l text-gray-800 mg-auto  ">Thanks for subscribing to our newsletter.</h3>
-<h2 className="text-l text-gray-800 mg-auto font-bold  ">Enjoy Reading our Articles.</h2>
-
-</div>
+        <h3 className="  text-sm  md:text-lg text-gray-800 mg-auto  ">
+          Thanks for subscribing to our newsletter.
+        </h3>
+        <h2 className="  text-lg text-gray-800 mg-auto font-bold  ">
+          Enjoy Reading our Articles.
+        </h2>
+      </div>
     </div>
-  )
+  );
 }
